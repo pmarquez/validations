d8545193a39f35519d478ffd0c23fdfb210a844c
Paulo Márquez Herrero <me@pmarquezh.info>
2022-08-26 18:28:57 +0200

    First Commit

---
+++ src/test/java/io/pmarquezh/training/validations/ValidationsApplicationTests.java
@@ -0,0 +1,13 @@
+package io.pmarquezh.training.validations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValidationsApplicationTests {
+
+	@Test
+	void contextLoads ( ) {
+	}
+
+}
